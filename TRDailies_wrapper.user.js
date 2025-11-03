4bcc314d3f03b5141312c44d1378282fec294fe3
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-03 02:05:41 +0000

    Update TRDailies_wrapper.user.js

--- TRDailies_wrapper.user.js
+++ TRDailies_wrapper.user.js
@@ -6,9 +6,9 @@
 // @author       TeamRocket
 // @icon         https://i.ibb.co/RptK4TP5/teamrocketlogo.gif
 // @match        https://*.tribalwars.com.pt/game.php*
-// @require      https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/TRDailies.js
-// @updateURL    https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/TRDailies_wrapper.js
-// @downloadURL  https://raw.githubusercontent.com/jpsilva95/TribalWarsDailiesTR/main/TRDailies_wrapper.js
+// @require      https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies.js
+// @updateURL    https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_wrapper.js
+// @downloadURL  https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_wrapper.js
 // @grant        GM_xmlhttpRequest
 // @connect      discord.com
 // ==/UserScript==
