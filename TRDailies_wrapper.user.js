b63ab55230b52fc51de8a2c6119fe08c9bdb03a4
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-03 06:18:45 +0000

    Update TRDailies_wrapper.user.js

--- TRDailies_wrapper.user.js
+++ TRDailies_wrapper.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Team Rocket Dailies
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Send daily farm and resource stats to Discord webhook
 // @author       Team Rocket
 // @icon         https://i.ibb.co/RptK4TP5/teamrocketlogo.gif
 // @match        https://*.tribalwars.com.pt/game.php*
 // @require      https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies.js
-// @updateURL    https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_wrapper.js
-// @downloadURL  https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_wrapper.js
+// @updateURL    https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_wrapper.user.js
+// @downloadURL  https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_wrapper.user.js
 // @grant        GM_xmlhttpRequest
 // @connect      discord.com
 // ==/UserScript==
