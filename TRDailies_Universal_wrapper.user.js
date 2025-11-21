e6cf37ba42ccd805afe8c1d49c7cfe4d0ddcea1b
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-21 23:43:01 +0000

    Update version number to 2.0

--- TRDailies_Universal_wrapper.user.js
+++ TRDailies_Universal_wrapper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Team Rocket Dailies
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  Send daily farm and resource stats to Discord webhook
 // @author       Team Rocket©
 // @icon         https://i.ibb.co/RptK4TP5/teamrocketlogo.gif
