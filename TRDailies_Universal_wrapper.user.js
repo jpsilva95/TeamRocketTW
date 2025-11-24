c8ede7ddbe01fe7f4306450ff2a9220bf1e4e430
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-24 16:17:47 +0000

    Rename script and update metadata for authorship

--- TRDailies_Universal_wrapper.user.js
+++ TRDailies_Universal_wrapper.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Team Rocket Dailies
+// @name         Mystery Dailies
 // @namespace    http://tampermonkey.net/
-// @version      2.9
+// @version      3.0
 // @description  Send daily farm and resource stats to Discord webhook
-// @author       Team Rocket©
-// @icon         https://i.ibb.co/RptK4TP5/teamrocketlogo.gif
+// @author       Mystery Inc.
+// @icon         https://i.ibb.co/x8JQX8yS/ex1.png
 // @match        https://*.tribalwars.com.pt/game.php*
 // @require      https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_Universal.js
 // @updateURL    https://raw.githubusercontent.com/jpsilva95/TeamRocketTW/main/TRDailies_Universal_wrapper.user.js
@@ -13,4 +13,4 @@
 // @connect      discord.com
 // ==/UserScript==
 
-//   © 2025 Team Rocket©. All Rights Reserved
+//   © 2025 Mystery Inc.©. All Rights Reserved
