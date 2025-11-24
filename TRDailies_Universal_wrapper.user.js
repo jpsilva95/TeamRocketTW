ab603d6474666246630b947c26d5f1611c8d1dd2
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-24 21:17:17 +0000

    Bump version from 3.2 to 3.3

--- TRDailies_Universal_wrapper.user.js
+++ TRDailies_Universal_wrapper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mystery Dailies
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      3.3
 // @description  Send daily farm and resource stats to Discord webhook
 // @author       Mystery Inc.
 // @icon         https://i.ibb.co/x8JQX8yS/ex1.png
